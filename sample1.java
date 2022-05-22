9ef65a99b9e57e5f59374d2e9055dcdd4f9dd8fa
WRITTUTORIAL <33227274+jemin777@users.noreply.github.com>
2022-05-22 14:40:15 +0530

    Create sample1.java

---
+++ sample1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class sample1
+
+{
+
+static void main(String args[])
+{ 
+
+
+
+
+for (int i=0;i<100;i+=10)
+
+{
+
+System.out.println(""+i);
+
+
+}
+
+
+
+
+ }
+
+
+
+ }
